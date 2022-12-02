546b3f42cbd34273270df9d4b8a653030506d29d
Jorzik <77500356+Jorzik@users.noreply.github.com>
2022-12-02 14:08:59 +0100

    refactoring: created obtain data

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Callable
 from data import Data
 import algorithms as alg
 from enum import Enum
+import numpy as np
 
 
 class Algs(Enum):
@@ -18,30 +19,49 @@ use_algorithm: Callable[[Algs, Data], alg.Algorithm] = lambda x, y: x.value(y)
 def main() -> None:
     """Start >>>"""
 
+    AMOUNT_OF_ERRORS: int = 1
+    data_milestones: dict[str, np.ndarray] = {}
+
     create_divider()
 
     # obtain the data
-    data: Data = Data("0")
-    print(f"original:  {data.content}")
+    data: Data
+    data, data_milestones = obtain_data(data_milestones)
 
     # prepare the data
     used_algorithm: alg.Algorithm = use_algorithm(Algs.PARITY_CHECKING, data)
 
     used_algorithm.prepare()
-    print(f"prepared:  {data.content}")
+    prepared_data: np.ndarray = data.content
 
     #'send' the data
-    data.send(1)
-    print(f"sent:      {data.content}")
+    data.send(AMOUNT_OF_ERRORS)
+    transferred_data: np.ndarray = data.content
 
     # retrieve the data
     valid: bool = used_algorithm()
+    received_data: np.ndarray
     if valid:
         print("valid data")
     else:
-        print(f"retrieved: {data.content}")
+        received_data = data.content
+
+    # display the results
+    print(f"original:  {data_milestones['original_data']}")
+    print(f"prepared:  {prepared_data}")
+    print(f"sent:      {transferred_data}")
+    print(f"retrieved: {received_data}")
 
     create_divider()
+
+
+def obtain_data(data_milestones: dict) -> tuple:
+    """obtains the data and returns the first milestone"""
+
+    data: Data = Data("0")
+    data_milestones["original_data"] = data.original
+
+    return data, data_milestones
 
 
 def create_divider() -> None:
